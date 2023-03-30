d92a045876273f14c1eaa6db86b415d88beef352
azawhat <azamat.erezhepbayev@gmail.com>
2023-03-31 02:27:03 +0600

    A function that finds minimum

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         int[] arr = {10,7,32,3,5};
         int min = findMin(arr, arr.length);
-        System.out.println("Minimum is " + min);
+        System.out.println("Minimum number is " + min);
     }
     public static int findMin(int[] arr, int n) {
         if (n == 1) {
