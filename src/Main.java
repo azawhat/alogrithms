51a7c7f2d2b7390e0039b99acb8072285046c1a5
azawhat <azamat.erezhepbayev@gmail.com>
2023-04-05 02:58:09 +0600

    Reverse array

--- src/Main.java
+++ src/Main.java
@@ -53,7 +53,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Length ");
         int n = sc.nextInt();
-        System.out.println("Write"+ " elements:");
+        System.out.println("Write "+ " elements:");
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
         ReverseArray.reverseArray(arr);
